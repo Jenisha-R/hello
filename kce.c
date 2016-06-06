2e32972c84cea24892dc3db470165de6adc956ad
Jenisha-R <jenisharamanathan95@gmail.com>
2016-06-06 11:37:08 +0530

    Create kce.c

---
+++ kce.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	printf("positive");
+	else if(n<0)
+	printf("negative");
+	else
+	printf("zero");
+	return 0;
+}
